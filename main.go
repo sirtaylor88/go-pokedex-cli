421d6875ab794c17142687d16152bc03cb76ca5d
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2025-01-07 21:29:22 +0100

    ✨ Add help and exit command.

--- main.go
+++ main.go
@@ -14,6 +14,37 @@ func cleanInput(text string) []string {
 	return words
 }
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var table = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Displays a help message",
+		callback:    commandHelp,
+	},
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	return nil
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
@@ -22,7 +53,18 @@ func main() {
 		input := scanner.Text()
 		formattedInput := cleanInput(input)
 		if len(formattedInput) > 0 {
-			fmt.Printf("Your command was: %s\n", formattedInput[0])
+			cmd := formattedInput[0]
+			command, ok := table[cmd]
+			if !ok {
+				fmt.Println("Unknow command.")
+			}
+			command.callback()
+			if cmd == "help" {
+				fmt.Println("")
+				for key, value := range table {
+					fmt.Printf("%s: %s\n", key, value.description)
+				}
+			}
 		} else {
 			fmt.Println("Unknow command.")
 		}
